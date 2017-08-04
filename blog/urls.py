53b77b3b9a077145dde7b2f6dbffd120687c03de
yuvicky <yuvicky24@gmail.com>
2017-08-04 11:03:50 -0700

    forum: extend-your-application

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+
 urlpatterns = [
     url(r'^$', views.question_list, name='question_list'),
+    url(r'^question/(?P<pk>\d+)/$', views.question_detail, name='question_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+def question_detail(request, pk):
+	question = get_object_or_404(Question, pk=pk)
+	return render(request, 'boldHER/question_detail.html', {'question': question})
+
 def question_list(request):
     questions = Question.objects.all()
     return render(request, 'boldHER/question_list2.html', {'questions': questions})
